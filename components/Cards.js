1d7f517a905c2dec112c2fa6ea4e953440af860f
DavidHall-Code <david_m_hall@outlook.com>
2020-10-30 12:34:18 -0400

    finished card.js starting stretch

--- components/Cards.js
+++ components/Cards.js
@@ -28,45 +28,62 @@ axios
     .then((ref) => {
         console.log('Here is your data', ref)
 
-        const data = ref.data.articles
-        console.log(data)
-
-        const bootstrap = data.bootstrap
-        const javascript = data.javascript
-        const technology = data.technology
-        const jquery = data.jquery
-        const node = data.node
-        console.log(node)
-        
+        const data = ref.data.articles;
+        console.log (data);
+        const bootstrap = data.bootstrap;
+        const javascript = data.javascript;
+        const technology = data.technology;
+        const jquery = data.jquery;
+        const node = data.node;
+        console.log(node);
+     
         node.forEach((el) => {
-            cardsEntryPoint.appendChild(cardMaker(el))
-        })
-        jquery.forEach((el) => {
-            cardsEntryPoint.appendChild(cardMaker(el))
-        })
-        technology.forEach((el) => {
-            cardsEntryPoint.appendChild(cardMaker(el))
-        })
+            cardsEntryPoint.appendChild(cardMaker(el));
+        });    
         javascript.forEach((el) => {
-            cardsEntryPoint.appendChild(cardMaker(el))
-        })
+            cardsEntryPoint.appendChild(cardMaker(el));
+        });
         bootstrap.forEach((el) => {
-            cardsEntryPoint.appendChild(cardMaker(el))
-        })
+            cardsEntryPoint.appendChild(cardMaker(el));
+        });
+        technology.forEach((el) => {
+            cardsEntryPoint.appendChild(cardMaker(el));
+        });
+        jquery.forEach((el) => {
+            cardsEntryPoint.appendChild(cardMaker(el));
+        });
+     
+    })
     .catch((err) => {
-        console.log('There was an error with the request', err)
-    })
+         console.log('There was na error with your request: ', err);
+    });
 
-    function cardMaker(object {
-        const divCard = document.createElement('div')
-        const headlineDiv = document.createElement('div')
-        const authorDiv = document.createElement('div')
-        const imgDiv = document.createElement('div')
-        const img = document.createElement('img')
-        const span = document.createElement('span')
 
-        
+ function cardMaker(object) {
+     const divCard = document.createElement('div');
+     const headlineDiv = document.createElement('div');
+     const authorDiv = document.createElement('div');
+     const imgDiv = document.createElement('div');
+     const img = document.createElement('img');
+     const span = document.createElement('span');
 
-    })
+     divCard.classList.add('card');
+     headlineDiv.classList.add('headline');
+     authorDiv.classList.add('author');
+     imgDiv.classList.add('img-container');
 
-    })
+     headlineDiv.textContent = object.headline;
+     img.src = object.authorPhoto;
+     span.textContent = `By ${object.authorName}`;
+     divCard.appendChild(headlineDiv);
+     divCard.appendChild(authorDiv);
+     authorDiv.appendChild(imgDiv);
+     imgDiv.appendChild(img);
+     authorDiv.appendChild(span);
+
+     divCard.addEventListener('click', (event) => {
+        console.log(headlineDiv.textContent = object.headline);
+     });
+     return divCard;
+ };
+ const cardsEntryPoint = document.querySelector('.cards-container');
